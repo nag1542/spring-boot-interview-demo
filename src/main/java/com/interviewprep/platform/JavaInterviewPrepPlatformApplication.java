9909b26dfeb91e966f7a30c72bf7d4fe07e237ec
nag1542 <nag1542@gmail.com>
2026-06-09 14:40:18 +0530

    added async changes

--- src/main/java/com/interviewprep/platform/JavaInterviewPrepPlatformApplication.java
+++ src/main/java/com/interviewprep/platform/JavaInterviewPrepPlatformApplication.java
@@ -2,10 +2,12 @@ package com.interviewprep.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableMethodSecurity
+@EnableAsync
 public class JavaInterviewPrepPlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaInterviewPrepPlatformApplication.class, args);
